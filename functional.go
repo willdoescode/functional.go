13f0f1d62790ba5b91095cc6105d2b1b102f13e7
Will Lane <williamlane923@gmail.com>
2021-09-11 14:03:23 -0700

    tests

--- functional.go
+++ functional.go
@@ -8,10 +8,40 @@ func Map[T any, A any](f func(T) A, l []T) []A {
 	return res
 }
 
+func Sum(l []int) int {
+	return Reduce(l, 0, func(prev int, curr int) int {
+		return prev + curr
+	})
+}
+
 func Reduce[T any, A any](l []T, initVal A, f func(A, T) A) A {
 	if len(l) == 0 {
 		return initVal
 	}
 	first, rest := l[0], l[1:]
 	return Reduce(rest, f(initVal, first), f)
+}
+
+func Id[T any](t T) T { return t }
+
+func Any[T any](test func(T) bool, l []T) bool {
+	for _, x := range l {
+		if test(x) {
+			return true
+		}
+	}
+	return false
+}
+
+type Pair[T any, A any] struct {
+	A T
+	B A
+}
+
+func Zip[T any, A any](l1 []T, l2 []A) []Pair[T, A] {
+	var res []Pair[T, A]
+	for i, v := range l1 {
+		res = append(res, Pair[T, A]{A: v, B: l2[i]})
+	}
+	return res
 }
---
+++ functional_test.go
@@ -0,0 +1,67 @@
+package f
+
+import (
+	"reflect"
+	"testing"
+)
+
+func failMessage[T any, A any](t *testing.T, f string, correct T, actual A) {
+	t.Errorf("%v failed, expected %v but got %v.", f, correct, actual)
+}
+
+func TestMap(t *testing.T) {
+	x := []int{1, 2, 3, 4}
+	correct := []int{2, 4, 6, 8}
+	mapRes := Map(func(y int) int { return y * 2 }, x)
+
+	if !reflect.DeepEqual(mapRes, correct) {
+		failMessage(t, "Map", correct, mapRes)
+	}
+}
+
+func TestReduce(t *testing.T) {
+	x := []int{1, 2, 3, 4}
+	mapRes := Reduce(x, 0, func(r int, curr int) int { return r + curr })
+
+	if mapRes != 10 {
+		failMessage(t, "Map", 10, mapRes)
+	}
+}
+
+func TestSum(t *testing.T) {
+	x := []int{1, 2, 3, 4}
+	if Sum(x) != 10 {
+		failMessage(t, "Sum", 10, Sum(x))
+	}
+}
+
+func TestZip(t *testing.T) {
+	x := []int{1, 2, 3, 4}
+	y := []int{2, 4, 6, 8}
+	correct := []Pair[int, int]{
+		{A: 1, B: 2},
+		{A: 2, B: 4},
+		{A: 3, B: 6},
+		{A: 4, B: 8},
+	}
+	zipRes := Zip(x, y)
+
+	if !reflect.DeepEqual(zipRes, correct) {
+		failMessage(t, "Zip", correct, zipRes)
+	}
+}
+
+func TestAny(t *testing.T) {
+	x := []int{1, 4, 3, 7, 9}
+	anyRes := Any(func(y int) bool { return y%2 == 0 }, x)
+
+	if !anyRes {
+		failMessage(t, "Any", true, false)
+	}
+}
+
+func TestId(t *testing.T) {
+	if Id(5) != 5 {
+		failMessage(t, "Id", 5, Id(5))
+	}
+}
