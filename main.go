e39f309d6a28746a81b610c85b8d6a7263caab00
Will Lane <williamlane923@gmail.com>
2021-09-11 13:03:04 -0700

    init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func Map[T any, A any](f func(T) A, l []T) []A {
+	var res []A
+	for _, x := range l {
+		res = append(res, f(x))
+	}
+	return res
+}
+
+func Reduce[T any, A any](l []T, initVal A, f func(A, T) A) A {
+	if len(l) == 0 {
+		return initVal
+	}
+	first, rest := l[0], l[1:]
+	return Reduce(rest, f(initVal, first), f)
+}
+
+func main() {
+	x := []int{1, 2, 3, 4}
+	fmt.Println(Map(func(x int) int {
+		return x * 2
+	}, x))
+
+	fmt.Println(Reduce(x, 0, func(r int, curr int) int {
+		return curr + r
+	}))
+}
